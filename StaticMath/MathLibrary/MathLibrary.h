085322e89acf5688f0438742a9f80c051df4d16a
fexy <95139985+fexy11@users.noreply.github.com>
2023-08-26 18:34:01 +0300

    Add files via upload

---
+++ StaticMath/MathLibrary/MathLibrary.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace MathLibrary
+{
+	class Arithmetic
+	{
+	public:
+		static double Add(double a, double b);
+
+		static double Subtract(double a, double b);
+
+		static double Multiply(double a, double b);
+
+		static double Divide(double a, double b);
+	};
+}
